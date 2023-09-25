39efbbb464448eb6c43b70c4b0de85d9c15cb595
Sang Phạm <youlovelong123@gmail.com>
2023-09-25 15:47:25 +0700

    CRUD API basic done

--- lib/api.dart
+++ lib/api.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+
 const String urlLeads =
     'https://dev.longphatcrm.vn/Api/index.php/V8/module/Leads';
 const String urlCreate = 'https://dev.longphatcrm.vn/Api/index.php/V8/module';
@@ -7,3 +9,25 @@ const String clientId = '31395009-38f9-8155-37aa-650a5a82a2d0';
 const String clientSecret = 'rO2GElsqtAojbXzRt9ts';
 const String username = 'nv';
 const String password = '12131415@!';
+
+Future<String> getAccessToken(Dio dio) async {
+  final request = dio.post(
+    urlToken,
+    data: {
+      'grant_type': grantType,
+      'client_id': clientId,
+      'client_secret': clientSecret,
+      'username': username,
+      'password': password,
+    },
+  );
+
+  final response = await request;
+
+  if (response.statusCode == 200) {
+    final token = response.data['access_token'];
+    return token;
+  } else {
+    throw Exception('Failed to get access token: ${response.statusCode}');
+  }
+}
--- lib/leads.dart
+++ lib/leads.dart
@@ -29,38 +29,22 @@ class LeadsPage extends StatefulWidget {
 
 class _LeadsPageState extends State<LeadsPage> {
   List<Map<String, dynamic>> leadsData = [];
+  late String accessToken;
 
   @override
   void initState() {
     super.initState();
-    fetchData();
+    getAccessTokenAndFetchData();
   }
 
-  Future<String> getAccessToken(Dio dio) async {
-    final request = dio.post(
-      urlToken,
-      data: {
-        'grant_type': grantType,
-        'client_id': clientId,
-        'client_secret': clientSecret,
-        'username': username,
-        'password': password,
-      },
-    );
-
-    final response = await request;
-
-    if (response.statusCode == 200) {
-      final token = response.data['access_token'];
-      return token;
-    } else {
-      throw Exception('Failed to get access token: ${response.statusCode}');
-    }
+  Future<void> getAccessTokenAndFetchData() async {
+    final dio = Dio();
+    accessToken = await getAccessToken(dio);
+    fetchData();
   }
 
   Future<void> fetchData() async {
     final dio = Dio();
-    final accessToken = await getAccessToken(dio);
 
     final request = dio.get(
       urlLeads,
@@ -80,6 +64,26 @@ class _LeadsPageState extends State<LeadsPage> {
     });
   }
 
+  void openLeadDetailsScreen(Map<String, dynamic> lead) async {
+    final updatedLead = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LeadDetailsPage(lead: lead),
+      ),
+    );
+
+    if (updatedLead != null) {
+      // Update the lead details if they were edited
+      final index =
+          leadsData.indexWhere((element) => element['id'] == updatedLead['id']);
+      if (index != -1) {
+        setState(() {
+          leadsData[index] = updatedLead;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -90,7 +94,7 @@ class _LeadsPageState extends State<LeadsPage> {
             Navigator.pushReplacement(
               context,
               MaterialPageRoute(
-                builder: (context) => const MyApp(), // Trở về trang chính
+                builder: (context) => const MyApp(),
               ),
             );
           },
@@ -114,13 +118,12 @@ class _LeadsPageState extends State<LeadsPage> {
                     icon:
                         const Icon(Icons.info, color: Colors.deepPurpleAccent),
                     onPressed: () {
-                      // Mở màn hình mới để xem thêm thông tin về lead
+                      openLeadDetailsScreen(lead);
                     },
                   ),
                   IconButton(
                     icon: const Icon(Icons.delete, color: Colors.red),
                     onPressed: () {
-                      // Hiển ra thông báo bạn có muốn xóa ?
                       showDialog(
                         context: context,
                         builder: (context) => AlertDialog(
@@ -134,16 +137,10 @@ class _LeadsPageState extends State<LeadsPage> {
                             ),
                             TextButton(
                               onPressed: () async {
-                                // Xác định id của khách hàng tiềm năng
-                                final leadId = lead[
-                                    'id']; // Thay lead['id'] bằng cách lấy id thực tế từ dữ liệu của bạn
-
-                                // Tạo client
+                                final leadId = lead['id'] as String;
                                 final client = Dio();
-
-                                // Gửi request DELETE lên API để xóa khách hàng tiềm năng
                                 final response = await client.delete(
-                                  'https://dev.longphatcrm.vn/Api/index.php/V8/moduleLeads/$leadId',
+                                  'https://dev.longphatcrm.vn/Api/index.php/V8/module/Leads/$leadId',
                                   options: Options(
                                     headers: {
                                       'Authorization': 'Bearer $accessToken',
@@ -151,12 +148,9 @@ class _LeadsPageState extends State<LeadsPage> {
                                   ),
                                 );
 
-                                // Kiểm tra kết quả của request
                                 if (response.statusCode == 200) {
-                                  // Xóa thành công, cập nhật danh sách leads
                                   fetchData();
                                 } else {
-                                  // Xóa không thành công, hiển thị thông báo lỗi
                                   ScaffoldMessenger.of(context).showSnackBar(
                                     SnackBar(
                                       content: Text(
@@ -165,7 +159,6 @@ class _LeadsPageState extends State<LeadsPage> {
                                   );
                                 }
 
-                                // Đóng hộp thoại xác nhận
                                 Navigator.pop(context);
                               },
                               child: const Text('Có'),
@@ -201,7 +194,6 @@ class _LeadsPageState extends State<LeadsPage> {
               leading: const Icon(Icons.add),
               title: const Text('Create'),
               onTap: () {
-                // Điều hướng đến trang tạo lead khi ấn vào Create
                 Navigator.push(
                   context,
                   MaterialPageRoute(
@@ -214,25 +206,102 @@ class _LeadsPageState extends State<LeadsPage> {
               leading: const Icon(Icons.content_copy),
               title: const Text('Duplicate'),
               onTap: () {
-                // Xử lý khi người dùng chọn Duplicate
+                // Handle Duplicate
               },
             ),
             ListTile(
               leading: const Icon(Icons.delete),
               title: const Text('Delete'),
               onTap: () {
-                // Xử lý khi người dùng chọn Delete
+                // Handle Delete
               },
             ),
             ListTile(
               leading: const Icon(Icons.search),
               title: const Text('Search'),
               onTap: () {
-                // Xử lý khi người dùng chọn Search
+                // Handle Search
               },
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class LeadDetailsPage extends StatefulWidget {
+  final Map<String, dynamic> lead;
+
+  const LeadDetailsPage({Key? key, required this.lead}) : super(key: key);
+
+  @override
+  _LeadDetailsPageState createState() => _LeadDetailsPageState();
+}
+
+class _LeadDetailsPageState extends State<LeadDetailsPage> {
+  late TextEditingController fullNameController;
+  late TextEditingController phoneMobileController;
+
+  @override
+  void initState() {
+    super.initState();
+    fullNameController =
+        TextEditingController(text: widget.lead['attributes']['full_name']);
+    phoneMobileController =
+        TextEditingController(text: widget.lead['attributes']['phone_mobile']);
+  }
+
+  @override
+  void dispose() {
+    fullNameController.dispose();
+    phoneMobileController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chi tiết Lead'),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: TextField(
+              controller: fullNameController,
+              decoration: InputDecoration(labelText: 'Họ và tên'),
+            ),
+          ),
+          ListTile(
+            title: TextField(
+              controller: phoneMobileController,
+              decoration: InputDecoration(labelText: 'Số điện thoại'),
+            ),
+          ),
+          // Add more lead details here as needed
+          // Example:
+          // ListTile(
+          //   title: Text('Email: ${widget.lead['attributes']['email']}'),
+          // ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Save the updated lead details and return to the previous screen
+          final updatedLead = {
+            ...widget.lead,
+            'attributes': {
+              'full_name': fullNameController.text,
+              'phone_mobile': phoneMobileController.text,
+              // Add other attributes here
+            },
+          };
+          Navigator.pop(context, updatedLead);
+        },
+        child: Icon(Icons.save),
+        backgroundColor: Colors.deepPurple,
       ),
     );
   }
